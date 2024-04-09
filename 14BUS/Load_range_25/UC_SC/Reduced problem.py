d285c7ae329a1dd3a7a354df243d28373477d9f6
Dobby <119469809+Hexuan085@users.noreply.github.com>
2024-04-09 21:48:06 +0800

    Add files via upload

---
+++ 14BUS/Load_range_25/UC_SC/Reduced problem.py
@@ -0,0 +1,223 @@
+import cvxpy as cp
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import *
+import random
+import csv
+from datetime import datetime
+import time
+import warnings
+warnings.filterwarnings("ignore")
+
+#  load the PTDF, generator information, marginal cost, binding constraints indexes
+with open('PTDF14.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+    # print(rows)
+    a_ln = np.array(rows, dtype=float)
+    # print(a_ln[185][117])
+
+with open('GEN14.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+    # print(rows)
+    g_index = np.array(rows, dtype=int)
+    # print(g_index[1][0])
+
+with open('linear_marginal_cost.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+    # print(rows)
+    C = np.array(rows, dtype=float)
+    # print(C)
+
+with open('UC_sta_line_max.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+    # print(rows)
+    sta_line_max = np.array(rows, dtype=int)
+    # print(C)
+
+with open('UC_sta_line_min.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+    # print(rows)
+    sta_line_min = np.array(rows, dtype=int)
+    # print(C)
+
+with open('UC_sta_line_bound_max.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+    # print(rows)
+    sta_line_bound_max = np.array(rows, dtype=int)
+    # print(C)
+
+with open('UC_sta_line_bound_min.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+    # print(rows)
+    sta_line_bound_min = np.array(rows, dtype=int)
+    # print(C)
+
+C = C[0][0:5]
+
+# Topology of the used network
+num_samples = 200
+num_of_gen = 5
+num_buses = 14
+num_lines = 20
+
+# Coefficients of constraints
+m = np.ones((1, num_of_gen), dtype=float)
+e_right = np.diag(m[0])*5
+e_left = np.diag(m[0])*0.5
+D = np.diag(m[0])
+
+b = np.zeros((num_buses, num_of_gen), dtype=float)
+
+for g in range(num_of_gen):
+    b[g_index[g][0]-1][g] = 1
+
+reduce_max = sta_line_max.shape[1]  # Generate the reduced line flow constraints
+reduce_min = sta_line_min.shape[1]
+reduce_min_bound = sta_line_bound_min.shape[1]
+reduce_max_bound = sta_line_bound_max.shape[1]
+
+line_flow_limit = np.ones((1, num_lines), dtype=float) * 3.5
+line_flow_limit_max = np.ones((1, reduce_max), dtype=float) * 3.5
+line_flow_limit_min = np.ones((1, reduce_min), dtype=float) * 3.5
+line_flow_limit_max_bound = np.ones((1, reduce_max_bound), dtype=float) * 3.5
+line_flow_limit_min_bound = np.ones((1, reduce_min_bound), dtype=float) * 3.5
+
+# Generate the reduced PTDF matrix
+a_ln_max = []
+a_ln_min = []
+a_ln_min_bound = []
+a_ln_max_bound = []
+
+for l_index in sta_line_max:  # Upper bound coefficient matrix of line flow for the standard form
+    a_ln_max.append(a_ln[l_index][:])
+
+for l_index in sta_line_min:  # Upper bound coefficient matrix of line flow for the cost-driven form
+    a_ln_min.append(a_ln[l_index][:])
+
+for l_index in sta_line_bound_min:  # Lower bound coefficient matrix of line flow for the standard form
+    a_ln_min_bound.append(a_ln[l_index][:])
+
+for l_index in sta_line_bound_max:  # Lower bound coefficient matrix of line flow for the cost-driven form
+    a_ln_max_bound.append(a_ln[l_index][:])
+
+print('shape of a_ln', np.shape(a_ln_max), np.shape(a_ln_min), np.shape(a_ln_min_bound), np.shape(a_ln_max_bound))
+
+# Time recording parameters
+time_0 = 0
+time_1 = 0
+time_2 = 0
+
+load_status = -1
+percentage = 0.25
+l_center = 15 / num_buses
+
+for i in range(num_samples):
+    # Load generation
+    load_status = -load_status
+    load_left = np.ones((1, 7), dtype=float) * l_center
+    load_right = np.ones((1, 7), dtype=float) * l_center
+    # print(load_left)
+    load_random_left = np.random.uniform(0, percentage * l_center, (1, 7))
+    load_random_right = load_random_left[0][0:7]
+    # print(load_random_left)
+    load_left = load_left + load_random_left * load_status
+    load_right = load_right - load_random_right * load_status
+    # print(load_left[0])
+    load = list(load_left[0]) + list(load_right[0])
+    load = np.array(load)
+    # print(load)
+
+    # Model and solve original UC problem
+    start_time = time.time()
+    x = cp.Variable(num_of_gen)
+    u = cp.Variable(num_of_gen, integer=True)
+    q = cp.Variable(num_buses)
+    cost = cp.sum(C * x)
+    constraint = [q == b * x - load.reshape(-1, ),
+                  cp.sum(q) == 0,  # Generation constraint
+                  e_left * u <= D * x,
+                  D * x <= e_right * u,
+                  u <= 1, u >= 0,
+                  a_ln * q >= -line_flow_limit.reshape(-1, ),
+                  line_flow_limit.reshape(-1, ) >= a_ln * q]  # line flow limits
+    try:
+        prob = cp.Problem(cp.Minimize(cost), constraint)
+        start_time = time.time()
+        prob.solve(solver='GLPK_MI')  # cp.CVXOPT
+        time_0 = time_0 + time.time() - start_time
+        print("Solution time CPLEX of original problem", time.time() - start_time)
+        if x.value is None:
+            # print("No solution!!!!!!")
+            # print("Impossible load combination", load)
+            print("Load sum", np.sum(load))
+            # continue
+    except KeyError:
+        print("KeyError")
+        # continue
+    print('cost', cost.value)
+
+    # Model and solve reduced UC problem obtained by standard form
+    x_reduce_0 = cp.Variable(num_of_gen)
+    u_reduce_0 = cp.Variable(num_of_gen, integer=True)
+    q_reduce_0 = cp.Variable(num_buses)
+    cost_reduce_0 = cp.sum(C * x_reduce_0)
+    constraint_reduce_0 = [q_reduce_0 == b * x_reduce_0 - load.reshape(-1, ),
+                           cp.sum(q_reduce_0) == 0,  # Generation constraint
+                           e_left * u_reduce_0 <= D * x_reduce_0,
+                           D * x_reduce_0 <= e_right * u_reduce_0,
+                           u_reduce_0 <= 1, u_reduce_0 >= 0,
+                           a_ln_min[0] * q_reduce_0 >= -line_flow_limit_min.reshape(-1, ),
+                           line_flow_limit_max.reshape(-1, ) >= a_ln_max[0] * q_reduce_0]  # line flow limits
+    try:
+        prob = cp.Problem(cp.Minimize(cost_reduce_0), constraint_reduce_0)
+        start_time = time.time()
+        prob.solve(solver='GLPK_MI')  # cp.CVXOPT
+        time_1 = time_1 + time.time() - start_time
+        print("Solution time CPLEX of reduced problem 1", time.time() - start_time)
+        if x.value is None:
+            # print("No solution!!!!!!")
+            # print("Impossible load combination", load)
+            print("Load sum", np.sum(load))
+            # continue
+    except KeyError:
+        print("KeyError")
+        # continue
+    print('cost_reduce_1', cost_reduce_0.value)
+
+    # Model and solve reduced UC problem obtained by cost-driven form
+    x_reduce_1 = cp.Variable(num_of_gen)
+    u_reduce_1 = cp.Variable(num_of_gen, integer=True)
+    q_reduce_1 = cp.Variable(num_buses)
+
+    cost_reduce_1 = cp.sum(C * x_reduce_1)
+
+    constraint_reduce_1 = [q_reduce_1 == b * x_reduce_1 - load.reshape(-1, ),
+                           cp.sum(q_reduce_1) == 0,  # Generation constraint
+                           e_left * u_reduce_1 <= D * x_reduce_1,
+                           D * x_reduce_1 <= e_right * u_reduce_1,
+                           u_reduce_1 <= 1, u_reduce_0 >= 0,
+                           a_ln_min_bound[0] * q_reduce_1 >= -line_flow_limit_min_bound.reshape(-1, )]
+    try:
+        prob = cp.Problem(cp.Minimize(cost_reduce_1), constraint_reduce_1)
+        start_time = time.time()
+        prob.solve(solver='GLPK_MI')  # cp.CVXOPT
+        time_2 = time_2 + time.time() - start_time
+        print("Solution time CPLEX of reduced problem 2", time.time() - start_time)
+        if x.value is None:
+            # print("No solution!!!!!!")
+            # print("Impossible load combination", load)
+            print("Load sum", np.sum(load))
+            # continue
+    except KeyError:
+        print("KeyError")
+        # continue
+    print('cost_reduce_2', cost_reduce_1.value)
+
+print('time_0, time_1, time_2', time_0/time_0, time_1/time_0, time_2/time_0)
---
+++ 14BUS/Load_range_50/Method A/Cost_fitting_14BUS.py
@@ -0,0 +1,210 @@
+#Code December 1st
+#Cost 1, 1.8, 2.5
+
+import tensorflow as tf
+import cvxpy as cp
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+#from util import *
+import random
+import csv
+random.seed(32)
+from sklearn.metrics import mean_squared_error
+from keras.layers import Dense, Dropout, Activation, Flatten
+from sklearn.metrics import mean_squared_error
+from keras.constraints import non_neg
+import time
+import os
+from keras.models import Sequential  # formulating NN model
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+from tensorflow.python.framework import graph_util
+from keras.models import load_model
+
+batch_size = 40
+learning_rate = 0.005
+training_epochs = 40
+num_of_buses = 14
+
+
+def scaled_gradient(x, predictions):
+    grad, = tf.gradients(predictions, x)
+    return grad
+
+def normal_model(input_dim, output_dim):
+    input = keras.layers.Input(shape=(input_dim,))
+    x0 = keras.layers.Dense(80, activation='relu')(input)
+    x1 = keras.layers.Dense(50, activation='relu')(x0)
+    x2 = keras.layers.Dense(30, activation='relu')(x1)
+    out = keras.layers.Dense(output_dim, kernel_constraint=non_neg())(x2)
+    model = keras.models.Model(inputs=input, outputs=out)
+    return model
+
+
+def keras_model_dnn(input_dim, output_dim):
+    model = Sequential()
+    model.add(Dense(100, input_dim=input_dim))
+    model.add(Activation('relu'))
+    model.add(Dense(50))
+    model.add(Activation('relu'))
+    model.add(Dense(30))
+    model.add(Activation('relu'))
+    model.add(Dense(output_dim))
+    return model
+
+with open('linear_data_all14_UC.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    rows = [row for row in reader]
+    data_all = np.array(rows, dtype=float)
+
+print("data shape", np.shape(data_all))
+X = data_all[:data_all.shape[0], :num_of_buses]  # load
+Y = data_all[:data_all.shape[0], -1]  # cost
+
+#X = data_all[10000:13000, :num_of_buses]
+#Y = data_all[10000:13000, -1]
+print("The last data sample instance", X[-1])
+
+max_valuex = np.max(X, axis=0)
+min_valuex = np.min(X, axis=0)
+max_valuey = np.max(Y, axis=0)
+min_valuey = np.min(Y, axis=0)
+
+train_X = (np.copy(X) - min_valuex) / (max_valuex - min_valuex)  # normalization
+train_Y = (np.copy(Y) - min_valuey) / (max_valuey - min_valuey)
+
+print("Training x maximum", max_valuex)
+print("Training x minimum", min_valuex)
+print("Training y maximum", max_valuey)
+print("Training y minimum", min_valuey)
+
+
+
+num_samples = train_X.shape[0]
+index = np.arange(num_samples)
+
+X_train = np.copy(train_X[index[:int(0.8*num_samples)]])  # num_train=0.8*num_samples
+Y_train = np.copy(train_Y[index[:int(0.8*num_samples)]])
+test_X = np.copy(train_X[index[int(0.8*num_samples):]])   # num_test=0.2*num_samples
+test_Y = np.copy(train_Y[index[int(0.8*num_samples):]])
+
+# layers = [14, 100, 10, 1]
+x_val = tf.placeholder(tf.float32, shape=(None, num_of_buses), name="load")
+y_val = tf.placeholder(tf.float32, shape=(None, 1))
+cost_vec = tf.placeholder(tf.float32, shape=(None, num_of_buses))
+
+#Trial for keras model
+model = keras_model_dnn(input_dim=num_of_buses, output_dim=1)
+predictions = model(x_val)
+sess = tf.Session()
+cost = tf.reduce_sum(tf.pow(predictions - y_val, 2)) / (2 * batch_size)  # calculate total y error.
+grad_pre = tf.gradients(predictions, x_val)# grad
+
+loss = tf.reduce_mean(tf.square(predictions - y_val))
+
+optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)  # 最小化总误差
+
+init = tf.global_variables_initializer()
+saver = tf.train.Saver()
+with tf.Session() as sess:
+    # Initializing the Variables
+    sess.run(init)
+    #keras.bakend.set_session(sess)
+    model.compile(loss='mean_absolute_error', optimizer='adam')
+    model.fit(X_train, Y_train, batch_size=32, epochs=30, shuffle=True)  # validation_split=0.1 训练模型
+
+    pred_val = model.predict(test_X)
+
+    RMSE = mean_squared_error(pred_val[:200], test_Y[:200])
+
+    plt.plot(pred_val[:200], 'r', linewidth=3)
+    plt.plot(test_Y[:200], 'b')
+    plt.show()  # 测试集的效果
+
+    for epoch in range(training_epochs):
+        #start_time = time.time()
+        for iterations in range(int(num_samples*0.8/batch_size-1)):
+            _x = X_train[iterations*batch_size:(iterations+1)*batch_size]  #batch
+            _y = Y_train[iterations*batch_size:(iterations+1)*batch_size].reshape(-1, 1)
+            sess.run(optimizer,
+                     feed_dict={x_val: _x, y_val: _y})
+            # 代入参数值开始训练
+        if (epoch + 1) % 2 == 0:
+            c = sess.run(cost, feed_dict={x_val: _x, y_val: _y})  # calculate total y error
+
+            print("Epoch", (epoch + 1), "cost loss= ", c)
+
+    pred_val=sess.run(predictions, feed_dict={x_val: test_X})
+
+    RMSE = mean_squared_error(pred_val[:200], test_Y[:200])
+
+    plt.plot(pred_val[:200], 'r', linewidth=3)
+    plt.plot(test_Y[:200], 'b')  # 测试效果
+    plt.show()
+
+    num = 0
+    cost_record = []
+    for i in range(0,  num_samples):  # Starting sample: 500, 900, 24380
+        current_x = np.copy(train_X[i]).reshape(1, -1)
+        #print("Current load", current_x * max_valuex)
+        #print("original costs", train_Y[i] * max_valuey)
+        #print("Predicted costs", (sess.run(predictions, feed_dict={x_val: current_x})) * (max_valuey))
+        # print("Current load", current_x * (max_valuex - min_valuex) + min_valuex)
+        # print("original costs", train_Y[i] * (max_valuey - min_valuey) + min_valuey)
+        # print("Predicted costs", (sess.run(predictions, feed_dict={x_val: current_x})) * (max_valuey - min_valuey) + min_valuey)
+        # print("next cost", train_Y[i+1])
+        cost_record.append((sess.run(predictions, feed_dict={x_val: current_x})) * (max_valuey - min_valuey) + min_valuey)
+        start_time = time.time()
+        solution_time = time.time() - start_time
+        num += 1
+
+    e = 0.05
+    # print(np.sum(current_x))
+    # print(current_x)
+    load = current_x
+    # print(load)
+    cost_max_0 = 0.1
+    cost_max_1 = 0
+
+    status_limit = 0
+    k = 0
+    percentage = 0.5
+    l_center = 15 / num_of_buses
+    while (cost_max_0 - cost_max_1) >= 0.0001 and k <= 5000:
+        k += 1
+        cost_max_1 = cost_max_0
+        grad_0 = sess.run(grad_pre, feed_dict={x_val: load})
+        grad_0 = np.array(grad_0)
+        index = np.argsort(grad_0)
+        index = index[0][0]
+        grad_status = 0
+        for i in index[::-1]:
+            if (load[0][i]) * (max_valuex[i] - min_valuex[i]) + min_valuex[i] + e <= l_center + percentage * l_center:
+                load[0][i] = (((load[0][i]) * (max_valuex[i] - min_valuex[i]) + min_valuex[i] + e) - min_valuex[i]) / (
+                            max_valuex[i] - min_valuex[i])
+                grad_status = 1
+                break
+            else:
+                status_limit += 1
+        for j in index:
+            if grad_status == 0:
+                break
+            elif load[0][j] * (max_valuex[j] - min_valuex[j]) + min_valuex[j] - e >= l_center - percentage * l_center:
+                load[0][j] = ((load[0][j] * (max_valuex[j] - min_valuex[j]) + min_valuex[j] - e) - min_valuex[j]) / (
+                            max_valuex[j] - min_valuex[j])
+                break
+            else:
+                status_limit += 1
+
+        cost_max_0 = (sess.run(predictions, feed_dict={x_val: load})) * (max_valuey - min_valuey) + min_valuey
+        # print(cost_max_0)
+        cost_max_0 = (cost_max_0[0])[0]
+    print(cost_max_0)
+    print(k)
+    print(status_limit)
+    print(max(cost_record))
+    print(load * (max_valuex - min_valuex) + min_valuex)
+    with open('linear_load_test.csv', 'w', newline='') as f:
+         writer = csv.writer(f)
+         writer.writerows(current_x * (max_valuex - min_valuex) + min_valuex)
